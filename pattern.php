547c144bd2e2b8f9f1b8575fb1dcab7414600b8b
dhavalsave <dhavalsave1071@gmail.com>
2020-01-31 11:57:00 +0530

    Update pattern.php

--- pattern.php
+++ pattern.php
@@ -1,41 +1,44 @@
-<?php
-$string="1A2B3C4D5EE5D4C3B2A1";
-$str_len=strlen($string);
-$str1=substr($string,0,$str_len/2);
-$str2=strrev($str1);
-$str_len=strlen($str1);
+$string = "1A2B3C4D5EE5D4C3B2A1";
+$str_len = strlen($string);
+$str1 = substr($string, 0, $str_len / 2);
+$str2 = strrev($str1);
+$str_len = strlen($str1);
 function printSpace($n)
 {
-    $s="";
-    while ($n>0)
-    {
-        $s=$s." ";
+    $s = "";
+    while ($n > 0) {
+        $s = $s . " ";
         $n--;
     }
-    return$s;
+    return $s;
 }
-$space="";
-$print="";
-for($j=$str_len,$i=0;$j>0,$i<$str_len;$j-=2,$i+=2)
-{
-    $print=substr($str1,0,$j);
-    $print=$print.printSpace($i);
-    $print=$print.printSpace($i);
-    $print=$print.substr($str2,$i,$str_len);
-    echo $print;
-    echo("\n");
 
+$space = "";
+$print = "";
+for ($j = $str_len, $i = 0; $j > 0, $i < $str_len; $j -= 2, $i += 2) {
+    $print = substr($str1, 0, $j);
+    $print = $print . printSpace($i);
+    $print = $print . printSpace($i);
+    $print = $print . substr($str2, $i, $str_len);
+    echo $print;
+
+        echo("\n");
 
 
 }
-for($i=$str_len,$j=0;$i>0,$j<$str_len;$i-=2,$j+=2)
-{
-    $print=substr($str1,0,$j);
-    $print=$print.printSpace($i);
-    $print=$print.printSpace($i);
-    $print=$print.substr($str2,$i,$str_len);
-    echo $print;
-    echo("\n");
+for ($i = $str_len, $j = 0; $i >= 0, $j <= $str_len; $i -= 2, $j += 2) {
+    if ($j > 2) {
+        $print = substr($str1, 0, $j);
+        $print = $print . printSpace($i);
+        $print = $print . printSpace($i);
+        $print = $print . substr($str2, $i, $str_len);
+        echo $print;
+        echo("\n");
+
+    }
+
+
+}
 
 
 
