53355dc3316e9edb44f2f548121bb4f4291326f7
Tejas <tejas.k@smartlybuilt.com>
2020-02-03 12:41:10 +0530

    Fixed exception

--- Bank/src/Account.php
+++ Bank/src/Account.php
@@ -1,81 +1,56 @@
 <?php
 
-class LessBalanceException extends Exception
-{
-    public function errorMessage($amount)
-    {
-        return "having balance less than Rs." . $amount;
+class LessBalanceException extends Exception {
+
+    public function __construct($message = "", $code = 0, Throwable $previous = null) {
+        $message = "Cannot withdraw Rs. " . $message . ". Account has less balance";
+        parent::__construct($message, $code, $previous);
     }
 }
 
-class Account extends LessBalanceException
-{
+class Account extends LessBalanceException {
+
     public $name;
     public $balance = 0;
 
-    public function createAccount($name, $openingBalance)
-    {
+    public function createAccount($name, $openingBalance) {
         if ($openingBalance < 500) {
-            throw new Exception("minimum Opening balance is Rs.500");
+            throw new Exception("Minimum Opening Balance is Rs. 500");
         }
+
         if (strlen($name) < 5) {
             throw new Exception("Enter full name");
         }
 
-        $this->name = $this->name;
+        $this->name = $name;
         $this->balance = $openingBalance;
-        echo "account Created : $name" . PHP_EOL;
+        echo "Account Created : $name" . PHP_EOL;
     }
 
-    public function deposit($amount, $name)
-    {
-        if (1) {
-            $this->balance += $amount;
-            echo("Rs." . $amount . " credited in account. Updated Balance:Rs." . $this->balance);
-
-        } else
-            echo("wrong User \n");
+    public function deposit($amount) {
+        $this->balance += $amount;
+        echo "Rs." . $amount . " Credited in account. Updated Balance: Rs. " . $this->balance . PHP_EOL;
     }
 
-    public function withDraw($name, $amount)
-    {
-        try {
-            if (0) {
-
-                //throw new Exception("Name Dosent Match\n");
-            }
-            echo 1/0;
-            if ($amount + 500 <= $this->balance) {
-                $this->balance -= $amount;
-                echo("Rs." . $amount . " Withdraw from account. Updated Balance:Rs." . $this->balance . "\n");
-
-            }
-        } catch (DivisionByZeroError $ex) {
-            echo "AAAAAAAAA";
-            exit;
-            echo $ex->errorMessage($amount);
-        }  catch (ArgumentCountError $ex) {
-            echo "AAAAAAAAA";
-            exit;
-            echo $ex->errorMessage($amount);
+    public function withDraw($amount) {
+        if ($this->balance - $amount < 500) {
+            throw new LessBalanceException($amount);
         }
+
+        $this->balance -= $amount;
+        echo "Rs." . $amount . " debited from account. Updated Balance: Rs." . $this->balance . PHP_EOL;
     }
 
-    public function printBalance()
-    {
-        echo($this->name . "having balance" . $this->balance . "\n");
+    public function printBalance() {
+        echo $this->name . "having balance" . $this->balance . PHP_EOL;
     }
 }
 
-//$a = new Account();
-//$a->createAccount("dhaval save", 5000);
-//$a->printBalance();
-
-$b = new Account();
-$b->createAccount("vibhor bhai", 5000);
-//$b->printBalance();
-//$a->deposit(200,"dhaval save");
-//$a->printBalance();
-$b->withDraw("vibhor bhai", 4600);
-$b->printBalance();
-
+try {
+    $b = new Account();
+    $b->createAccount("vibhor bhai", 5000);
+    $b->withDraw(4600);
+    $b->printBalance();
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+}
